60d1a1cbb016c1fb827c19bbc552c16d88e9d83e
Olga Bazhenova <olgabazhenova07@yandex.ru>
2021-01-07 23:59:35 +0300

    Переименовывает setup в init в nav.js

--- source/js/modules/nav.js
+++ source/js/modules/nav.js
@@ -4,19 +4,7 @@ class Nav {
     this.navToggle = navToggle;
   }
 
-  open() {
-    this.navElem.classList.remove(`main-nav--closed`);
-    this.navElem.classList.add(`main-nav--opened`);
-    this.navToggle.setAttribute(`aria-expanded`, true);
-  }
-
-  close() {
-    this.navElem.classList.remove(`main-nav--opened`);
-    this.navElem.classList.add(`main-nav--closed`);
-    this.navToggle.setAttribute(`aria-expanded`, false);
-  }
-
-  setup() {
+  init() {
     const onNavToggleClick = () => {
       if (this.navElem.classList.contains(`main-nav--closed`)) {
         this.open();
@@ -28,6 +16,18 @@ class Nav {
     this.navElem.classList.remove(`main-nav--nojs`);
     this.close();
     this.navToggle.addEventListener(`click`, onNavToggleClick);
+  }
+
+  open() {
+    this.navElem.classList.remove(`main-nav--closed`);
+    this.navElem.classList.add(`main-nav--opened`);
+    this.navToggle.setAttribute(`aria-expanded`, true);
+  }
+
+  close() {
+    this.navElem.classList.remove(`main-nav--opened`);
+    this.navElem.classList.add(`main-nav--closed`);
+    this.navToggle.setAttribute(`aria-expanded`, false);
   }
 }
 
