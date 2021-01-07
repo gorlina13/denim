9f5b96544bd856456c9a8ef4debe7976287a53b1
Olga Bazhenova <olgabazhenova07@yandex.ru>
2021-01-07 23:58:13 +0300

    Добавляет init(selector, Component) в main.js

--- source/js/main.js
+++ source/js/main.js
@@ -2,19 +2,42 @@ import 'picturefill';
 import 'element-closest-polyfill';
 import Nav from './modules/nav';
 
-function manageNav() {
-  const navElem = document.querySelector(`.main-nav`);
-  const navToggle = document.querySelector(`.main-nav__toggle`);
-  const innerPage = !navElem.classList.contains(`main-nav--index`);
+function init(selector, Component) {
+  const initNav = () => {
+    const navElem = document.querySelector(selector);
+    const navToggle = navElem.querySelector(`button`);
 
-  if (navElem !== null && navToggle !== null && innerPage) {
-    const nav = new Nav(navElem, navToggle);
-    nav.setup();
+    if (navElem !== null && navToggle !== null) {
+      const nav = new Component(navElem, navToggle);
+      nav.init();
+    }
+  };
+
+  const initInstances = () => {
+    const elems = document.querySelectorAll(selector);
+    if (elems.length > 0) {
+      elems.forEach((item) => {
+        const instance = new Component(item);
+        instance.init();
+      });
+    }
+  };
+
+  const name = Component.prototype.constructor.name;
+
+  switch (name) {
+    case `Nav`:
+      initNav();
+      break;
+
+    default:
+      initInstances();
+      break;
   }
 }
 
 function work() {
-  manageNav();
+  init(`.main-nav`, Nav);
 }
 
 if (document.readyState === `loading`) {
