52fd8b68b18d76717bcb39007234c1b409f2b54a
Olga Bazhenova <olgabazhenova07@yandex.ru>
2021-03-23 17:08:11 +0300

    Добавляет onRangeFormReset, выделяет setCSSVariable как метод

--- source/js/modules/range.js
+++ source/js/modules/range.js
@@ -1,17 +1,33 @@
 class Range {
   constructor(elem) {
     this.elem = elem;
+    this.inputs = elem.querySelectorAll(`input[type="range"]`);
+    this.form = this.inputs[0].form;
   }
 
   init() {
-    this.elem.addEventListener(`input`, this.onRangeInput);
+    this.elem.addEventListener(`input`, this.onRangeInput.bind(this));
+    if (this.form !== undefined) {
+      this.form.addEventListener(`reset`, this.onRangeFormReset.bind(this));
+    }
+  }
+
+  setCSSVariable(input) {
+    this.elem.style.setProperty(`--${input.id}`, input.value);
+  }
+
+  onRangeFormReset() {
+    const timer = setTimeout(() => {
+      this.inputs.forEach((input) => this.setCSSVariable(input));
+      clearTimeout(timer);
+    }, 0);
   }
 
   onRangeInput(evt) {
     const target = evt.target;
 
     if (target.tagName === `INPUT` && target.type === `range`) {
-      target.parentElement.style.setProperty(`--${target.id}`, target.value);
+      this.setCSSVariable(target);
     }
   }
 }
